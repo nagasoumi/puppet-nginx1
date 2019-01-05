ef40e70ed3989d9459185d4c2ee798c1d0d85715
rivethead42 <rivethead42@gmail.com>
2019-01-05 20:55:04 +0000

    first commit

--- install.pp
+++
@@ -1,9 +0,0 @@
-class nginx1::install(
-  String $ensure =       $::nginx1::ensure,
-  String $package_name = $::nginx1::package_name,
-) {
-  package { 'install_nginx':
-    ensure => $ensure,
-    name   => $package_name,
-  }
-}
